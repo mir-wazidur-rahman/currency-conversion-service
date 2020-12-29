3faa7dec77470a2162e670022e7701a21862f08d
Mir <mir.wazid.rahman@gmail.com>
2020-12-29 15:05:51 +0000

    curreny-conversion-example

---
+++ src/main/java/com/test/microservices/currencyconversionmicroservice/CurrencyConversionMicroserviceApplication.java
@@ -0,0 +1,26 @@
+package com.test.microservices.currencyconversionmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+@SpringBootApplication
+@EnableFeignClients("com.test.microservices.currencyconversionmicroservice")
+@EnableDiscoveryClient
+public class CurrencyConversionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionMicroserviceApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler  defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE ;
+	}
+
+}
---
+++ src/test/java/com/test/microservices/currencyconversionmicroservice/CurrencyConversionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.microservices.currencyconversionmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
